bbacfdfc5c370bb3fca30faf49e57f2f5bfecf82
Natalia <idmitrievanm@gmail.com>
2019-05-31 11:14:45 +1200

    Added JSON functionality and 10 questions

--- talktome_quiz_bot.py
+++ talktome_quiz_bot.py
@@ -1,6 +1,7 @@
 """This script (Telegram bot) prompts a user to check his English level
  using a standard English quiz"""
 
+import json
 import telebot
 from telebot import types
 
@@ -20,22 +21,15 @@ class TalkToMeQuizBot:
         # variable for counting questions
         self.question_number = 1
 
-        # dictionaries with data for quiz
-        self.questions = {1 : "Q1: Tom _________ English",
-                          2 : "Q2: _________ there a restaurant near here?",
-                          3 : "Q3: I didn't _________ TV last night."}
-
-        self.answers = {1 : ("is", "am", "are", "be"),
-                        2 : ("Have", "Is", "Do", "Are"),
-                        3 : ("not watched", "watched", "watch", "watching")}
-
-        self.right_answers = {0 : "none", 1 : "is", 2 : "Is", 3 : "watch"}
+        # dictionary with data for quiz from JSON file
+        with open("quiz.json", "r") as read_file:
+            self.quiz = json.load(read_file)
 
         self.results = {}
 
     def checking_answer(self, answer, chat_id, score, question_number):
         """A function for checking right answers"""
-        if answer == self.right_answers[question_number - 1]:
+        if answer == self.quiz["questions"][str(int(question_number) - 1)]["right"]:
             score += 1
         self.results[chat_id] = (score, question_number)
 
@@ -54,20 +48,25 @@ def send_welcome(message):
 def send_question(message):
     """A function for sending questions to a user"""
     chat_id = message.chat.id
-    this_question_number = QUIZ.results[chat_id][1]
+    this_question_number = str(QUIZ.results[chat_id][1])
 
     QUIZ.checking_answer(message.text, chat_id, QUIZ.results[chat_id][0], this_question_number)
-    if this_question_number <= len(QUIZ.questions):
+    if int(this_question_number) < int(len(QUIZ.quiz["questions"])):
         markup = types.ReplyKeyboardMarkup()
-        markup.row(QUIZ.answers[this_question_number][0], QUIZ.answers[this_question_number][1])
-        markup.row(QUIZ.answers[this_question_number][2], QUIZ.answers[this_question_number][3])
-        BOT.send_message(chat_id, QUIZ.questions[this_question_number], reply_markup=markup)
-        this_question_number += 1
+        markup.row(QUIZ.quiz["questions"][this_question_number]["A"],
+                   QUIZ.quiz["questions"][this_question_number]["B"])
+        markup.row(QUIZ.quiz["questions"][this_question_number]["C"])
+        BOT.send_message(chat_id,
+                         QUIZ.quiz["questions"][this_question_number]["question"],
+                         reply_markup=markup)
+        this_question_number = int(this_question_number) + 1
         QUIZ.results[chat_id] = (QUIZ.results[chat_id][0], this_question_number)
     else:
         markup = types.ReplyKeyboardRemove(selective=False)
         BOT.send_message(message.chat.id,
-                         'Your score is: ' + str(QUIZ.results[chat_id][0]) + '/3',
+                         'Your score is: ' +
+                         str(QUIZ.results[chat_id][0]) + '/' +
+                         str(len(QUIZ.quiz["questions"])-1),
                          reply_markup=markup)
         QUIZ.results[chat_id] = (QUIZ.results[chat_id][0], this_question_number)
 
